81925ffdc16729f3437812c6170960329c3d2e4f
Wojciech Przybyła <contact@wpww.pl>
2023-05-09 13:08:30 +0200

    Randoms can scroll too!

--- resources/views/sets/present.blade.php
+++ resources/views/sets/present.blade.php
@@ -5,15 +5,15 @@
 <div class="container" id="root"></div>
 <script src="{{ asset('/js/set.js') }}"></script>
 
-@auth
 <div class="flex-right stretch">
+    @auth
     <a href="{{ route('set', ['set_id' => $set->id]) }}" class="flex-right stretch">
         <x-button>Edytuj mszę</x-button>
     </a>
+    @endauth
     <x-button onclick="window.scrollTo({top: 0, behavior: 'smooth'});">
         Na początek
     </x-button>
 </div>
-@endauth
 
 @endsection
