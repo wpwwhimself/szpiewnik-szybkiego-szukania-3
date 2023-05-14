a1ee010e012b54cc8457a45027e0cde193fa59b1
Wojciech Przybyła <contact@wpww.pl>
2023-05-14 13:50:39 +0200

    songs - colored every new initial

--- resources/views/songs/list.blade.php
+++ resources/views/songs/list.blade.php
@@ -10,11 +10,12 @@
 
 @foreach ($categories as $cat)
     <h1 class="cap-initial">{{ $cat->name }}</h1>
-    <div class="flex-right wrap center boldEm">
+    <div class="flex-right wrap center">
     @foreach ($songs[$cat->name] as $song)
-        <a href="{{ route('song', ['title_slug' => Str::slug($song->title)]) }}">
+        <a href="{{ route('song', ['title_slug' => Str::slug($song->title)]) }}" class="{{ substr($song->title, 0, 1) != ($initial ?? '') ? 'boldEm' : '' }}">
             {{ $song->title }}
         </a>
+        @php $initial = substr($song->title, 0, 1) @endphp
     @endforeach
     </div>
 @endforeach
