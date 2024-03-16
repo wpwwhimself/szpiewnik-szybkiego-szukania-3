916948979af114f1cf160f64bec0956262bafbc8
Wojciech Przybyła <contact@wpww.pl>
2024-03-16 21:11:10 +0100

    🚚 created react app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wpww.sz3',
   appName: 'Szpiewnik Szybkiego Szukania',
-  webDir: 'public/build',
+  webDir: 'frontend/public',
   server: {
     androidScheme: 'https'
   }
