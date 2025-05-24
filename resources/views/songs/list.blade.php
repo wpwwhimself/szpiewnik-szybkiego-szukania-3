5fa857e812310ae022b2e54789f3bc8f92245cd7
Wojciech Przybyła <contact@wpww.pl>
2025-05-24 19:52:24 +0200

    🚸 songs | added search to song list

--- resources/views/songs/list.blade.php
+++ resources/views/songs/list.blade.php
@@ -8,6 +8,30 @@
 </a>
 @endif
 
+<search>
+    <x-input type="text" name="search" label="Szukaj po tytule" oninput="searchSongs(event)" />
+</search>
+
+<script>
+function searchSongs(event) {
+    const q = event.target.value;
+
+    // hide songs not matching search
+    document.querySelectorAll(`#main-wrapper .list-element`).forEach(el => {
+        const hide = (q != "")
+            ? !(new RegExp(q, "i").test(el.querySelector(`.main`).textContent))
+            : false;
+        el.classList.toggle("hidden", hide);
+    });
+
+    // cleanup headings (hide if all songs are hidden)
+    document.querySelectorAll(`#main-wrapper > h1`).forEach(el => {
+        const hide = el.nextElementSibling.querySelectorAll(`.list-element:not(.hidden)`).length == 0;
+        el.classList.toggle("hidden", hide);
+    });
+}
+</script>
+
 @foreach ($categories as $cat)
     <h1 class="cap-initial">{{ $cat->name }}</h1>
     <div class="flex-right wrap center">
