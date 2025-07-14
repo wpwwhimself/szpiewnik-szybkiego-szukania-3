a56c2bb08a7c292961afc163110522ff18cd6f42
Wojciech Przybyła <contact@wpww.pl>
2025-07-14 22:39:57 +0200

    🚸 songs | added filters of parts

--- resources/views/songs/list.blade.php
+++ resources/views/songs/list.blade.php
@@ -9,12 +9,49 @@
 @endif
 
 <search>
-    <x-input type="text" name="search" label="Szukaj po tytule" oninput="searchSongs(event)" />
+    <x-input type="text" name="search" label="Szukaj po tytule" oninput="searchSongs()" />
+
+    <div class="flex-right center">
+        <label>Szukaj po przeznaczeniu</label>
+        @foreach ([
+            ["Wejście", 0],
+            ["Dary", 2],
+            ["Komunia", 4],
+            ["Uwielbienie", 6],
+            ["Zakończenie", 8],
+            ["Pozostałe", "*"],
+        ] as [$label, $pref_index])
+        <span role="filter" class="button slick" onclick="showSongsByPref('{{ $pref_index }}', this);">{{ $label }}</span>
+        @endforeach
+        <span role="filter-reset" class="button hidden" onclick="filterReset()">↺</span>
+    </div>
 </search>
 
 <script>
-function searchSongs(event) {
-    const q = event.target.value;
+//* helpers *//
+function cleanUpHeadings() {
+    // cleanup headings (hide if all songs are hidden)
+    document.querySelectorAll(`#main-wrapper > h1`).forEach(el => {
+        const hide = el.nextElementSibling.querySelectorAll(`.list-element:not(.hidden)`).length == 0;
+        el.classList.toggle("hidden", hide);
+    });
+}
+
+function activateFilterBtn(btn = undefined) {
+    document.querySelectorAll(`[role='filter'].accent-border`)?.forEach(el => el.classList.remove("accent-border"));
+    btn?.classList.add("accent-border");
+    showFilterResetBtn();
+}
+
+function showFilterResetBtn(visible = true) {
+    document.querySelector(`[role='filter-reset']`)?.classList.toggle("hidden", !visible);
+}
+
+//* search functions *//
+function searchSongs() {
+    filterReset();
+
+    const q = document.querySelector("input[name='search']").value;
 
     // hide songs not matching search
     document.querySelectorAll(`#main-wrapper .list-element`).forEach(el => {
@@ -24,11 +61,34 @@ function searchSongs(event) {
         el.classList.toggle("hidden", hide);
     });
 
-    // cleanup headings (hide if all songs are hidden)
-    document.querySelectorAll(`#main-wrapper > h1`).forEach(el => {
-        const hide = el.nextElementSibling.querySelectorAll(`.list-element:not(.hidden)`).length == 0;
+    cleanUpHeadings();
+}
+
+function showSongsByPref(pref_index, btn) {
+    searchReset();
+
+    document.querySelectorAll(`#main-wrapper .list-element`).forEach(el => {
+        const prefs = el.querySelector(`.main`).dataset.prefs;
+        const hide = (pref_index != "*")
+            ? prefs.charAt(pref_index) != "1"
+            : prefs.substring(0, 9) != "0/0/0/0/0";
         el.classList.toggle("hidden", hide);
     });
+
+    cleanUpHeadings();
+    activateFilterBtn(btn);
+}
+
+function searchReset() {
+    document.querySelector("input[name='search']").value = "";
+}
+
+function filterReset() {
+    activateFilterBtn();
+    showFilterResetBtn(false);
+
+    document.querySelectorAll(`#main-wrapper .list-element`).forEach(el => el.classList.remove("hidden"));
+    cleanUpHeadings();
 }
 </script>
 
@@ -40,7 +100,8 @@ function searchSongs(event) {
             :present="route('song-present', ['title_slug' => Str::slug($song->title)])"
             :edit="route('song', ['title_slug' => Str::slug($song->title)])"
             clearance-for-edit="2"
-            >
+            :data-prefs="$song->preferences"
+        >
             {{ $song->title }}
         </x-list-element>
         @php $initial = substr($song->title, 0, 1) @endphp
