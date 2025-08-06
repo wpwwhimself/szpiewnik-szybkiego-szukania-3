b49112d33f444112d3f1c60b2226e4e1ff91a6ed
Wojciech Przybyła <contact@wpww.pl>
2025-08-06 10:33:48 +0200

    🧹 sets | added ordering of dot counters by color ordering

--- resources/views/components/set-color-counter.blade.php
+++ resources/views/components/set-color-counter.blade.php
@@ -2,8 +2,13 @@
     "sets",
 ])
 
+@php
+$color_ordering = App\Models\OrdinariusColor::orderBy("ordering")->pluck("ordering", "name");
+$ordered_sets = $sets->groupBy("color")->sortBy(fn ($clr, $k) => $color_ordering[$k]);
+@endphp
+
 <div class="flex-right center middle ghost">
-    @foreach ($sets->groupBy("color")->sortKeys() as $color => $sets_of_color)
+    @foreach ($ordered_sets as $color => $sets_of_color)
     <span style="color: {{ $sets_of_color->first()->colorData->display_color }};">⬤</span>
     <x-dot-counter :number="$sets_of_color->count()" />
     @endforeach
