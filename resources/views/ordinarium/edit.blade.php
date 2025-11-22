3eb8cb31199bf9bff813c44797a7e3a308630a32
Wojciech Przybyła <contact@wpww.pl>
2025-11-22 23:29:12 +0100

    ♻️ updated ordinarius editor

--- resources/views/ordinarium/edit.blade.php
+++ resources/views/ordinarium/edit.blade.php
@@ -4,67 +4,70 @@
 
 @section("content")
 
-<h1>Parametry części stałej</h1>
-
-<form method="post" action="{{ route("ordinarius-edit") }}">
-    @csrf
+<x-shipyard.app.form method="post" :action="route('ordinarius-edit')">
     <script src="{{ asset("js/note-transpose.js") }}"></script>
 
+<x-shipyard.app.section title="Nuty" icon="music-note">
     <div id="variant-big-container">
     @foreach ($ordinarius->sheet_music_variants as $var_no => $notes)
-        <div class="variant-container">
-            <x-input type="TEXT" name="sheet_music[]" :var-no="$var_no" label="Nuty" value="{!! $notes !!}" />
-            <div id="note-transpose-{{ $var_no }}" class="flex right center wrap">
-                <x-shipyard.ui.button
-                    icon="music-note-plus"
-                    pop="Transponuj w górę"
-                    action="none"
-                    class="tertiary"
-                    name="up"
-                />
-                <x-shipyard.ui.button
-                    icon="music-note-minus"
-                    pop="Transponuj w dół"
-                    action="none"
-                    class="tertiary"
-                    name="down"
-                />
+        <div class="variant-container grid but-mobile-down" style="--col-count: 2;">
+            <x-input type="TEXT" name="sheet_music[]" :var-no="$var_no" label="Nuty" value="{!! $notes !!}" rows="5" />
+            <div>
+                <div id="note-transpose-{{ $var_no }}" class="flex right center wrap">
+                    <x-shipyard.ui.button
+                        icon="music-note-plus"
+                        pop="Transponuj w górę"
+                        action="none"
+                        class="tertiary"
+                        name="up"
+                        onclick="Hoch(this.closest(`.variant-container`).querySelector(`textarea[name^='sheet_music']`));"
+                    />
+                    <x-shipyard.ui.button
+                        icon="music-note-minus"
+                        pop="Transponuj w dół"
+                        action="none"
+                        class="tertiary"
+                        name="down"
+                        onclick="Runter(this.closest(`.variant-container`).querySelector(`textarea[name^='sheet_music']`));"
+                    />
+                </div>
+                <div id="sheet-music-container-{{ $var_no }}"></div>
+                <div class="flex right spread and-cover">
+                    <x-shipyard.ui.button id="remove-variant" :var-no="$var_no"
+                        label="Usuń wariant"
+                        icon="delete"
+                        action="none"
+                        class="tertiary"
+                        onclick="event.target.closest(`.variant-container`).remove();"
+                    />
+                </div>
             </div>
-            <div id="sheet-music-container-{{ $var_no }}"></div>
-            <div class="flex right spread and-cover">
-                <x-shipyard.ui.button id="remove-variant" :var-no="$var_no"
-                    label="Usuń wariant"
-                    action="none"
-                    class="danger"
-                />
-            </div>
-            <hr>
+            <hr style="grid-column: span 2">
         </div>
-        <script>
-        document.querySelector("#note-transpose-{{ $var_no }} button[name=up]").addEventListener("click", (e) => { e.preventDefault(); Hoch(document.querySelector("textarea[var-no='{{ $var_no }}']"), e); });
-        document.querySelector("#note-transpose-{{ $var_no }} button[name=down]").addEventListener("click", (e) => { e.preventDefault(); Runter(document.querySelector("textarea[var-no='{{ $var_no }}']"), e); });
-        document.querySelector("#remove-variant[var-no='{{ $var_no }}']").addEventListener("click", (e) => { e.preventDefault(); e.target.closest(".variant-container").remove(); })
-        </script>
     @endforeach
     </div>
+    <div class="flex right spread and-cover">
+        <x-shipyard.ui.button id="addVariantButton"
+            label="Dodaj wariant"
+            icon="plus"
+            action="none"
+            onclick="addVariant(event)"
+            class="tertiary"
+        />
+    </div>
+</x-shipyard.app.section>
 
     <input type="hidden" name="color_code" value="{{ $ordinarius->color_code }}" />
     <input type="hidden" name="part" value="{{ $ordinarius->part }}" />
 
-    <div class="flex right spread and-cover">
-        <x-shipyard.ui.button
-            label="Dodaj wariant"
-            class="tertiary"
-            action="none"
-            onclick="addVariant(event);"
-        />
+    <x-slot:actions>
         <x-shipyard.ui.button
             label="Zatwierdź i wróć"
             class="primary"
             action="submit"
         />
-    </div>
-</form>
+    </x-slot:actions>
+</x-shipyard.app.form>
 
 <script src="{{ asset("js/abcjs-basic-min.js") }}"></script>
 <script defer>
@@ -102,9 +105,6 @@ function addVariant(ev){
 
     render(newNoteInput);
     newNoteInput.addEventListener("keyup", () => render(newNoteInput));
-    newVariant.querySelector(`#note-transpose-${new_var_no} button[name=up]`).addEventListener("click", (e) => { e.preventDefault(); Hoch(document.querySelector(`textarea[var-no='${new_var_no}']`), e); });
-    newVariant.querySelector(`#note-transpose-${new_var_no} button[name=down]`).addEventListener("click", (e) => { e.preventDefault(); Runter(document.querySelector(`textarea[var-no='${new_var_no}']`), e); });
-    newVariant.querySelector(`#remove-variant`).addEventListener("click", (e) => { e.preventDefault(); e.target.closest(".variant-container").remove(); })
 }
 </script>
 
